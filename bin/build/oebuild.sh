6f394010913e56ddfba140c31231059199e4ad64
Holger Schurig <schurig@mn-solutions.de>
2003-05-03 20:49:36 +0000

    added lots of functions

--- bin/build/oebuild.sh
+++ bin/build/oebuild.sh
@@ -6,8 +6,112 @@ unalias -a
 # It _must_ preceed all the calls to die and assert.
 shopt -s expand_aliases
 alias die='diefunc "$FUNCNAME" "$LINENO" "$?"'
+alias assert='_retval=$?; [ $_retval = 0 ] || diefunc "$FUNCNAME" "$LINENO" "$_retval"'
+
+PATH="${OEDIR}/bin/build:${PATH}"
+test -z "$PREROOTPATH" || PATH="${PREROOTPATH%%:}:$PATH"
 
 
+# filter-flags <flag> ####
+#	Remove particular flags from C[XX]FLAGS
+
+filter-flags () {
+	for x in $1; do
+		export CFLAGS="${CFLAGS/${x}}"
+		export CXXFLAGS="${CXXFLAGS/${x}}"
+	done
+}
+
+
+# append-flags <flag>
+#	Add extra flags to your current C[XX]FLAGS
+
+append-flags () {
+	CFLAGS="${CFLAGS} $1"
+	CXXFLAGS="${CXXFLAGS} $1"
+}
+
+
+# replace-flags <orig.flag> <new.flag>
+#	Replace a flag by another one
+
+replace-flags () {
+	CFLAGS="${CFLAGS/${1}/${2} }"
+	CXXFLAGS="${CXXFLAGS/${1}/${2} }"
+}
+
+
+# is-flag <flag> ####
+#	Returns "true" if flag is set in C[XX]FLAGS
+#	Matches only complete flag
+
+is-flag() {
+	for x in ${CFLAGS} ${CXXFLAGS};	do
+		if [ "${x}" = "$1" ]; then
+			echo true
+			return 0
+		fi
+	done
+	return 1
+}
+
+
+# strip-flags
+#	Strip C[XX]FLAGS of everything except known good options.
+
+strip-flags() {
+	local NEW_CFLAGS=""
+	local NEW_CXXFLAGS=""
+
+	set -f
+	for x in ${CFLAGS}; do
+		for y in ${ALLOWED_FLAGS}; do
+			if [ "${x/${y}}" != "${x}" ]; then
+				if [ -z "${NEW_CFLAGS}" ]; then
+					NEW_CFLAGS="${x}"
+				else
+					NEW_CFLAGS="${NEW_CFLAGS} ${x}"
+				fi
+			fi
+		done
+	done
+
+	for x in ${CXXFLAGS}; do
+		for y in ${ALLOWED_FLAGS}; do
+			if [ "${x/${y}}" != "${x}" ]; then
+				if [ -z "${NEW_CXXFLAGS}" ]; then
+					NEW_CXXFLAGS="${x}"
+				else
+					NEW_CXXFLAGS="${NEW_CXXFLAGS} ${x}"
+				fi
+			fi
+		done
+	done
+
+	set +f
+
+	export CFLAGS="${NEW_CFLAGS}"
+	export CXXFLAGS="${NEW_CXXFLAGS}"
+}
+
+
+# get-flag <flag>
+#	Find and echo the value for a particular flag
+
+get-flag() {
+	local findflag="$1"
+
+	for f in ${CFLAGS} ${CXXFLAGS}; do
+		if [ "${f/${findflag}}" != "${f}" ]; then
+			echo "${f/-${findflag}=}"
+			return
+		fi
+	done
+}
+
+
+# TODO: oepatch
+# TODO?: drawline, oenewuser, oenewgroup, oedos2unix
 
 # return 0 when argument is in ${USE}
 
@@ -27,6 +131,10 @@ use() {
 
 
 # find out if the first argument is one of the rest of the arguments
+#
+#	if ! has test $FEATURES ; then
+#		...
+#	fi
 
 has() {
         local x
@@ -48,27 +156,10 @@ has() {
 
 
 
-#has_version() {
-#        # return shell-true/shell-false if exists.
-#        # Takes single depend-type atoms.
-#        if /usr/lib/portage/bin/portageq 'has_version' ${ROOT} $1; then
-#                return 0
-#        else
-#                return 1
-#        fi
-#}
-
-
-#best_version() {
-#        # returns the best/most-current match.
-#        # Takes single depend-type atoms.
-#        /usr/lib/portage/bin/portageq 'best_version' ${ROOT} $1
-#}
-
-
 use_with() {
         if [ -z "$1" ]; then
-                die "use_with() called without parameter"
+                oefatal "use_with() called without parameter"
+		return
         fi
 
         local UWORD="$2"
@@ -88,7 +179,8 @@ use_with() {
 
 use_enable() {
         if [ -z "$1" ]; then
-                die "use_with() called without parameter"
+                oefatal "use_with() called without parameter"
+		return
         fi
 
         local UWORD="$2"
@@ -110,26 +202,11 @@ diefunc() {
         local funcname="$1" lineno="$2" exitcode="$3"
         shift 3
         echo >&2
-        echo "ERROR: $CATEGORY/$PF failed" >&2
-        echo "NOTE: Function $funcname, Line $lineno, Exitcode $exitcode" >&2
-        echo "NOTE: ${*:-(no error message)}" >&2
-        echo >&2
+        oefatal "$CATEGORY/$PF failed"
+        oenote "Function $funcname, Line $lineno, Exitcode $exitcode"
+        oenote "${*:-(no error message)}"
         exit 1
 }
-
-
-#if no perms are specified, dirs/files will have decent defaults
-#(not secretive, but not stupid)
-umask 022
-export DESTTREE=/usr
-export INSDESTTREE=""
-export EXEDESTTREE=""
-export DOCDESTTREE=""
-export INSOPTIONS="-m0644"
-export EXEOPTIONS="-m0755"
-export LIBOPTIONS="-m0644"
-export DIROPTIONS="-m0755"
-export MOPREFIX=${PN}
 
 
 unpack() {
@@ -140,7 +217,7 @@ unpack() {
 	for x in $@
 	do
 		myfail="failure unpacking ${x}"
-		echo "NOTE: Unpacking ${x} to $(pwd)"
+		oenote "Unpacking ${x} to $(pwd)"
 		y="$(echo $x | sed 's:.*\.\(tar\)\.[a-zA-Z0-9]*:\1:')"
 		case "${x##*.}" in
 		tar) 
@@ -170,18 +247,18 @@ unpack() {
 			fi
 			;;
 		*)
-			echo "unpack ${x}: file format not recognized. Ignoring."
+			oefatal "unpack ${x}: file format not recognized"
 			;;
 		esac
 	done
 }
 
 
+#TODO: add crosscompile support
 oeconf() {
 	if [ -x ./configure ] ; then
-		if [ ! -z "${CBUILD}" ]; then
-			EXTRA_ECONF="--build=${CBUILD} ${EXTRA_ECONF}"
-		fi
+		test -z "${CBUILD}" || EXTRA_ECONF="--build=${CBUILD} ${EXTRA_ECONF}"
+		test -z "${CCHOST}" || EXTRA_ECONF="--target=${CCHOST} ${EXTRA_ECONF}"
 		./configure \
 		    --prefix=/usr \
 		    --host=${CHOST} \
@@ -190,7 +267,7 @@ oeconf() {
 		    --datadir=/usr/share \
 		    --sysconfdir=/etc \
 		    --localstatedir=/var/lib \
-				${EXTRA_ECONF} \
+			${EXTRA_ECONF} \
 		    "$@" || die "econf failed" 
 	else
 		die "no configure script found"
@@ -221,34 +298,156 @@ pkg_setup()
 
 pkg_nofetch()
 {
-	[ -z "${SRC_URI}" ] && return
+	test -z "${SRC_URI}" && return
 
-	echo "!!! The following are listed in SRC_URI for ${PN}:"
+	oenote "The following files are listed in SRC_URI for ${PN}:"
 	for MYFILE in `echo ${SRC_URI}`; do
-		echo "!!!   $MYFILE"
+		oenote "  $MYFILE"
 	done
-	return 
 }
 
 
+
 src_unpack() { 
-	if [ "${A}" != "" ]
-	then
-		unpack ${A}
-	fi	
+	test "${A}" != "" && unpack ${A}
 }
 
 
 src_compile() { 
 	if [ -x ./configure ] ; then
-		econf 
-		emake || die "emake failed"
+		oeconf 
+		oemake || die "oemake failed"
 	fi
 	return 
 }
+
 
 src_install() 
 { 
 	return 
 }
 
+
+try() {
+	env "$@"
+	if [ "$?" != "0" ]; then
+		oefatal "ERROR: the $1 command did not complete successfully."
+		oefatal "(\"$*\")"
+		oefatal "Since this is a critical task, ebuild will be stopped."
+		exit 1
+	fi
+}
+
+
+# debug-print() gets called from many places with verbose status information useful
+# for tracking down problems. The output is in $T/eclass-debug.log.
+# You can set OECLASS_DEBUG_OUTPUT to redirect the output somewhere else as well.
+# The special "on" setting echoes the information, mixing it with the rest of the
+# emerge output.
+# You can override the setting by exporting a new one from the console, or you can
+# set a new default in oe.conf. Here the default is "" or unset.
+
+debug-print() {
+	while [ "$1" ]; do
+	
+		# extra user-configurable targets
+		if [ "$OECLASS_DEBUG_OUTPUT" == "on" ]; then
+			echo "debug: $1"
+		elif [ -n "$OECLASS_DEBUG_OUTPUT" ]; then
+			echo "debug: $1" >> $OECLASS_DEBUG_OUTPUT
+		fi
+		
+		# default target
+		echo $1 >> ${T}/oeclass-debug.log
+		
+		shift
+	done
+}
+
+# The following 2 functions are debug-print() wrappers
+
+debug-print-function() {
+	str="$1: entering function" 
+	shift
+	debug-print "$str, parameters: $*"
+}
+
+debug-print-section() {
+	debug-print "now in section $*"
+}
+
+# Sources all eclasses in parameters
+inherit() {
+	unset INHERITED
+	local location
+	while [ "$1" ]
+	do
+		location="${OEDIR}/bin/build/${1}.oeclass"
+		POECLASS="$OECLASS"
+		export OECLASS="$1"
+
+		# TODO: work throught OEPATH
+		#if [ -n "$PORTDIR_OVERLAY" ]
+		#then
+		#	olocation="${PORTDIR_OVERLAY}/eclass/${1}.eclass"
+		#	if [ -e "$olocation" ]; then
+		#		location="${olocation}"
+		#	fi
+		#fi
+
+		debug-print "inherit: $1 -> $location"
+		source "$location" || die "died sourcing $location in inherit()"
+		OECLASS="$POECLASS"
+		unset POECLASS
+
+		if ! has $1 $INHERITED &>/dev/null; then
+			export INHERITED="$INHERITED $1"
+		fi
+
+		shift
+	done
+}
+
+
+# Exports stub functions that call the oeclass's functions, thereby making them default.
+# For example, if OECLASS="base" and you call "EXPORT_FUNCTIONS src_unpack", the following
+# code will be eval'd:
+# src_unpack() { base_src_unpack; }
+EXPORT_FUNCTIONS() {
+	if [ -z "$OECLASS" ]; then
+		oefatal "EXPORT_FUNCTIONS without a defined OECLASS"
+		exit 1
+	fi
+	while [ "$1" ]; do
+		debug-print "EXPORT_FUNCTIONS: ${1} -> ${OECLASS}_${1}" 
+		eval "$1() { ${OECLASS}_$1 ; }" >/dev/null
+		shift
+	done
+}
+
+
+# adds all parameters to DEPEND and RDEPEND
+newdepend() {
+	debug-print-function newdepend $*
+	debug-print "newdepend: DEPEND=$DEPEND RDEPEND=$RDEPEND"
+
+	while [ -n "$1" ]; do
+		case $1 in
+		"/autotools")
+			DEPEND="${DEPEND} sys-devel/autoconf sys-devel/automake sys-devel/make"
+			;;
+		"/c")
+			DEPEND="${DEPEND} sys-devel/gcc virtual/glibc"
+			RDEPEND="${RDEPEND} virtual/glibc"
+			;;
+		*)
+			DEPEND="$DEPEND $1"
+			if [ -z "$RDEPEND" ] && [ "${RDEPEND-unset}" == "unset" ]; then
+				export RDEPEND="$DEPEND"
+			fi
+			RDEPEND="$RDEPEND $1"
+			;;
+		esac
+		shift
+	done
+}
