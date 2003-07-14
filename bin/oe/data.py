cafefc92744d47e158e0cd9528c2045704a0f73b
Chris Larson <clarson@kergoth.com>
2003-07-14 17:54:20 +0000

    Update documentation, and make use of doctest, for the 'data' implementation.

--- bin/oe/data.py
+++ bin/oe/data.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
+"""
+OpenEmbedded 'Data' implementations
 
-# proposed new way of structuring environment data for the
-# OpenEmbedded buildsystem
+Functions for interacting with the data structure used by the
+OpenEmbedded (http://openembedded.org) build infrastructure.
 
-from oe import debug, note
+Copyright: (c) 2003 Chris Larson
+
+Based on functions from the base oe module, Copyright 2003 Holger Schurig
+"""
+
+import sys, os
+if sys.argv[0][-5:] == "pydoc":
+	path = os.path.dirname(os.path.dirname(sys.argv[1]))
+else:
+	path = os.path.dirname(os.path.dirname(sys.argv[0]))
+sys.path.append(path)
+
+from oe import note, debug
 
 def init():
 	return {}
@@ -19,20 +33,18 @@ def initVar(var, d = _data):
 		d[var]["flags"] = {}
 
 def setVar(var, value, d = _data):
-	"""Set a variable to a given value"""
+	"""Set a variable to a given value
+
+	Example:
+		>>> setVar('TEST', 'testcontents')
+		>>> print getVar('TEST')
+		testcontents
+	"""
 	for v in ["_append", "_prepend", "_delete"]:
 		match = re.match('(?P<base>.*?)%s(_(?P<add>.*))?' % v, var)
 		if match:
 			base = match.group('base')
 			override = match.group('add')
-			# add is only set in the case of
-			# base_cmd_override, in which case we set
-			# a parameter as a conditional on the cmd.
-#			if override:
-#				note("base_cmd_override instance: %s%s_%s" % (base, v, override))
-#			else:
-#				note("base[_override]_cmd instance: %s%s" % (base, v))
-
 			l = getVarFlag(base, v, d) or []
 			l.append([value, override])
 			setVarFlag(base, v, l, d)
@@ -45,7 +57,13 @@ def setVar(var, value, d = _data):
 		d[var]["content"] = value 
 
 def getVar(var, d = _data, exp = 0):
-	"""Gets the value of a variable"""
+	"""Gets the value of a variable
+
+	Example:
+		>>> setVar('TEST', 'testcontents')
+		>>> print getVar('TEST')
+		testcontents
+	"""
 	try:
 		if exp:
 			return expand(d[var]["content"], d)
@@ -55,11 +73,26 @@ def getVar(var, d = _data, exp = 0):
 		return None
 
 def delVar(var, d = _data):
-	"""Removes a variable from the data set"""
+	"""Removes a variable from the data set
+
+	Example:
+		>>> setVar('TEST', 'testcontents')
+		>>> print getVar('TEST')
+		testcontents
+		>>> delVar('TEST')
+		>>> print getVar('TEST')
+		None
+	"""
 	del d[var]
 
 def setVarFlag(var, flag, flagvalue, d = _data):
-	"""Set a flag for a given variable to a given value"""
+	"""Set a flag for a given variable to a given value
+
+	Example:
+		>>> setVarFlag('TEST', 'python', 1)
+		>>> print getVarFlag('TEST', 'python')
+		1
+	"""
 #	print "d[%s][\"flags\"][%s] = %s" % (var, flag, flagvalue)
 	try:
 		d[var]["flags"][flag] = flagvalue
@@ -68,18 +101,42 @@ def setVarFlag(var, flag, flagvalue, d = _data):
 		d[var]["flags"][flag] = flagvalue
 
 def getVarFlag(var, flag, d = _data):
-	"""Gets given flag from given var""" 
+	"""Gets given flag from given var
+
+	Example:
+		>>> setVarFlag('TEST', 'python', 1)
+		>>> print getVarFlag('TEST', 'python')
+		1
+	"""
 	try:
 		return d[var]["flags"][flag]
 	except KeyError:
 		return None
 
 def delVarFlag(var, flag, d = _data):
-	"""Removes a given flag from the variable's flags"""
+	"""Removes a given flag from the variable's flags
+
+	Example:
+		>>> setVarFlag('TEST', 'testflag', 1)
+		>>> print getVarFlag('TEST', 'testflag')
+		1
+		>>> delVarFlag('TEST', 'testflag')
+		>>> print getVarFlag('TEST', 'testflag')
+		None
+		
+	"""
 	del d[var]["flags"][flag]
 
 def setVarFlags(var, flags, d = _data):
-	"""Set the flags for a given variable"""
+	"""Set the flags for a given variable
+	
+	Example:
+		>>> myflags = {}
+		>>> myflags['test'] = 'blah'
+		>>> setVarFlags('TEST', myflags)
+		>>> print getVarFlag('TEST', 'test')
+		blah
+	"""
 	try:
 		d[var]["flags"] = flags
 	except KeyError:
@@ -87,14 +144,30 @@ def setVarFlags(var, flags, d = _data):
 		d[var]["flags"] = flags
 
 def getVarFlags(var, d = _data):
-	"""Gets a variable's flags"""
+	"""Gets a variable's flags
+
+	Example:
+		>>> setVarFlag('TEST', 'test', 'blah')
+		>>> print getVarFlags('TEST')['test']
+		blah
+	"""
 	try:
 		return d[var]["flags"]
 	except KeyError:
 		return None
 
 def delVarFlags(var, d = _data):
-	"""Removes a variable's flags"""
+	"""Removes a variable's flags
+
+	Example:
+		>>> setVarFlag('TEST', 'testflag', 1)
+		>>> print getVarFlag('TEST', 'testflag')
+		1
+		>>> delVarFlags('TEST')
+		>>> print getVarFlags('TEST')
+		None
+		
+	"""
 	del d[var]["flags"]
 
 def getData(d = _data):
@@ -111,39 +184,18 @@ __expand_var_regexp__ = re.compile(r"\${[^{}]+}")
 __expand_python_regexp__ = re.compile(r"\${@.+?}")
 
 def expand(s, d = _data):
-	"""Can expand variables with their values from env[]
+	"""Variable expansion using the data store.
 
-	>>> env['MID'] = 'drin'
-	>>> print expand('vorher ${MID} dahinter')
-	vorher drin dahinter
+	Example:
+		Standard expansion:
+		>>> setVar('A', 'sshd')
+		>>> print expand('/usr/bin/${A}')
+		/usr/bin/sshd
 
-	Unset variables are kept as is:
-
-	>>> print expand('vorher ${MID} dahinter ${UNKNOWN}')
-	vorher drin dahinter ${UNKNOWN}
-
-	A syntax error just returns the string:
-
-	>>> print expand('${UNKNOWN')
-	${UNKNOWN
-
-	We can evaluate python code:
-
-	>>> print expand('${@ "Test"*3}')
-	TestTestTest
-	>>> env['START'] = '0x4000'
-	>>> print expand('${@ hex(0x1000000+${START}) }')
-	0x1004000
-
-	We are able to handle recursive definitions:
-
-	>>> env['ARCH'] = 'arm'
-	>>> env['OS'] = 'linux'
-	>>> env['SYS'] = '${ARCH}-${OS}'
-	>>> print expand('${SYS}')
-	arm-linux
+		Python expansion:
+		>>> print expand('result: ${@37 * 72}')
+		result: 2664
 	"""
-
 	def var_sub(match):
 		key = match.group()[2:-1]
 		#print "got key:", key
@@ -179,13 +231,14 @@ def expandData(alterdata = _data, readdata = None):
 	"""For each variable in alterdata, expand it, and update the var contents.
 	   Replacements use data from readdata.
 
-	   Example:
-	   to = {}
-	   from = {}
-	   setVar("dlmsg", "dl_dir is ${DL_DIR}", to)
-	   setVar("DL_DIR", "/path/to/whatever", from)
-	   expandData(to, from)
-	   getVar("dlmsg", to) returns "dl_dir is /path/to/whatever"
+	Example:
+		>>> a=init()
+		>>> b=init()
+		>>> setVar("dlmsg", "dl_dir is ${DL_DIR}", a)
+		>>> setVar("DL_DIR", "/path/to/whatever", b)
+		>>> expandData(a, b)
+		>>> print getVar("dlmsg", a)
+		dl_dir is /path/to/whatever
 	   """
 	if readdata == None:
 		readdata = alterdata
@@ -201,6 +254,16 @@ def expandData(alterdata = _data, readdata = None):
 import os
 
 def inheritFromOS(pos, d = _data):
+	"""Inherit variables from the environment.
+
+	Example:
+		>>> d=init()
+		>>> os.environ["TEST"] = "test"
+		>>> setVarFlag('TEST', 'inherit', '1', d)
+		>>> inheritFromOS(1, d)
+		>>> print getVar('TEST', d)
+		test
+	"""
 	pos = str(pos)
 	for s in os.environ.keys():
 		try:
@@ -213,6 +276,7 @@ def inheritFromOS(pos, d = _data):
 import sys, string
 
 def emit_var(var, o=sys.__stdout__, d = _data):
+	"""Emit a variable to be sourced by a shell."""
 	if getVarFlag(var, "python", d):
 		return 0
 
@@ -236,12 +300,7 @@ def emit_var(var, o=sys.__stdout__, d = _data):
 
 
 def emit_env(o=sys.__stdout__, d = _data):
-	"""This prints the data so that it can later be sourced by a shell
-	Normally, it prints to stdout, but this it can be redirectory to some open file handle
-
-	It is used by exec_shell_func().
-	"""
-
+	"""Emits all items in the data store in a format such that it can be sourced by a shell."""
 	oedir = getVar('OEDIR', d)
 	if oedir is None:
 		oedir = "." 
@@ -269,67 +328,52 @@ def emit_env(o=sys.__stdout__, d = _data):
 		emit_var(e, o, d) and o.write('\n')
 
 def update_data(d = _data):
-	"""Modifies the environment vars according to local overrides
+	"""Modifies the environment vars according to local overrides and commands.
+	Examples:
+		Appending to a variable:
+		>>> setVar('TEST', 'this is a')
+		>>> setVar('TEST_append', ' test')
+		>>> setVar('TEST_append', ' of the emergency broadcast system.')
+		>>> update_data()
+		>>> print getVar('TEST')
+		this is a test of the emergency broadcast system.
 
-	For the example we do some preparations:
+		Prepending to a variable:
+		>>> setVar('TEST', 'virtual/libc')
+		>>> setVar('TEST_prepend', 'virtual/tmake ')
+		>>> setVar('TEST_prepend', 'virtual/patcher ')
+		>>> update_data()
+		>>> print getVar('TEST')
+		virtual/patcher virtual/tmake virtual/libc
 
-	>>> setenv('TEST_arm', 'target')
-	>>> setenv('TEST_ramses', 'machine')
-	>>> setenv('TEST_local', 'local')
-        >>> setenv('OVERRIDES', 'arm')
-
-	and then we set some TEST environment variable and let it update:
-
-	>>> setenv('TEST', 'original')
-	>>> update_env()
-	>>> print env['TEST']
-	target
-
-	You can set OVERRIDES to another value, yielding another result:
-
-        >>> setenv('OVERRIDES', 'arm:ramses:local')
-	>>> setenv('TEST', 'original')
-	>>> update_env()
-	>>> print env['TEST']
-	local
-
-	Besides normal updates, we are able to append text:
-
-	>>> setenv('TEST_append', ' foo')
-	>>> update_env()
-	>>> print env['TEST']
-	local foo
-
-	And we can prepend text:
-
-	>>> setenv('TEST_prepend', 'more ')
-	>>> update_env()
-	>>> print env['TEST']
-	more local foo
-
-	Deleting stuff is more fun with multiline environment variables, but
-	it works with normal ones, too. The TEST_delete='foo' construct
-	deletes any line in TEST that matches 'foo':
-
-	>>> setenv('TEST_delete', 'foo ')
-	>>> update_env()
-	>>> print "'%s'" % env['TEST']
-	''
+		Overrides:
+		>>> setVar('TEST_arm', 'target')
+		>>> setVar('TEST_ramses', 'machine')
+		>>> setVar('TEST_local', 'local')
+	        >>> setVar('OVERRIDES', 'arm')
+	
+		>>> setVar('TEST', 'original')
+		>>> update_data()
+		>>> print getVar('TEST')
+		target
+	
+	        >>> setVar('OVERRIDES', 'arm:ramses:local')
+		>>> setVar('TEST', 'original')
+		>>> update_data()
+		>>> print getVar('TEST')
+		local
 	"""
 
-	debug(2, "update_env()")
+	debug(2, "update_data()")
 
 	# can't do delete env[...] while iterating over the dictionary, so remember them
 	dodel = []
-	# preprocess overrides
-	overrides = expand(getVar('OVERRIDES', d), d)
-	if not overrides:
-		debug(1, "OVERRIDES not defined, nothing to do")
-		return
-	overrides = overrides.split(':')
+	overrides = string.split(getVar('OVERRIDES', d, 1) or "", ":") or []
 
 	def applyOverrides(var, d = _data):
-		overrides = string.split(getVar('OVERRIDES', d, 1) or "", ":") or []
+		if not overrides:
+			debug(1, "OVERRIDES not defined, nothing to do")
+			return
 		val = getVar(var, d)
 		flags = getVarFlags(var, d)
 		for o in overrides:
@@ -387,3 +431,12 @@ def update_data(d = _data):
 		delVar(s, d)
 
 	inheritFromOS(5)
+
+def _test():
+	"""Start a doctest run on this module"""
+	import doctest
+	from oe import data
+	doctest.testmod(data)
+
+if __name__ == "__main__":
+	_test()
