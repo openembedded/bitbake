5abb194ed4efe27146ed3aec7af538525e4eae7c
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2004-02-18 13:43:12 +0000

    dummy functions for NYI stuff

--- bin/commander/mainwindow.py
+++ bin/commander/mainwindow.py
@@ -1,21 +1,85 @@
 #!/usr/bin/env python
 # -*- coding: iso8859-15 -*-
 
+from appinfo import *
 from mainwindowbase import *
+from aboutdialog import *
 
 class MainWindow( MainWindowBase ):
-    pass
 
+    def __init__( self, parent = None, name = None, fl = 0 ):
+        MainWindowBase.__init__(self, parent, name, fl )
+
+    def fileNew(self):
+        print "MainWindowBase.fileNew(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def fileOpen(self):
+        print "MainWindowBase.fileOpen(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def fileSave(self):
+        print "MainWindowBase.fileSave(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def fileSaveAs(self):
+        print "MainWindowBase.fileSaveAs(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def filePrint(self):
+        print "MainWindowBase.filePrint(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def fileExit(self):
+        print "MainWindowBase.fileExit(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def editUndo(self):
+        print "MainWindowBase.editUndo(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def editRedo(self):
+        print "MainWindowBase.editRedo(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def editCut(self):
+        print "MainWindowBase.editCut(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def editCopy(self):
+        print "MainWindowBase.editCopy(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def editPaste(self):
+        print "MainWindowBase.editPaste(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def editFind(self):
+        print "MainWindowBase.editFind(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def helpIndex(self):
+        print "MainWindowBase.helpIndex(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def helpContents(self):
+        print "MainWindowBase.helpContents(): Not implemented yet"
+        QMessageBox.information( self, "%s V%s" % ( appname, appversion ), "Not implemented yet" )
+
+    def helpAbout(self):
+        d = AboutDialog()
+        d.exec_loop()
 
 #------------------------------------------------------------------------#
 # main
 #------------------------------------------------------------------------#
 
-import sys
-from qt import *
-app = QApplication( sys.argv )
-mw = MainWindow()
-mw.show()
-app.setMainWidget( mw )
-app.exec_loop()
+if __name__ == "__main__":
+    import sys
+    from qt import *
+    app = QApplication( sys.argv )
+    mw = MainWindow()
+    mw.show()
+    app.setMainWidget( mw )
+    app.exec_loop()
 
