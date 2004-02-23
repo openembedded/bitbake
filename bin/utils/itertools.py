ded2faf5ce54967b061e58bc60ec743ac9e77722
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2004-02-23 14:29:08 +0000

    make compatible with python 2.2

--- bin/utils/itertools.py
+++ bin/utils/itertools.py
@@ -1,5 +1,7 @@
 """itertools appeared in Python 2.3 - this module mimicks it (partly)"""
 
+from __future__ import generators
+
 def cycle( sequence ):
     """Return a cyclic generator iterating over sequence"""
     while True:
