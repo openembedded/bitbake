f32f931afd07a4484d9be8507246c1b2f2c57db6
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2004-02-26 17:08:38 +0000

    more info

--- bin/commander/appinfo.py
+++ bin/commander/appinfo.py
@@ -1,3 +1,8 @@
+import os
+hostname = os.uname()[1]
+hostos = os.uname()[2]
 appname = "OpenEmbedded Commander"
 appversion = "0.0.1"
-appcaption = "%s V%s" % ( appname, appversion )
+appcaption = "%s V%s on %s (%s)" % ( appname, appversion, hostname, hostos )
+imageDir = "%s/bin/commander/images/" % os.environ["OEDIR"]
+
