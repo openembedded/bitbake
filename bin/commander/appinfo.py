40627dafcfe7cc2f30b5ffa542eec32bed6e1fb7
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2004-02-27 00:03:36 +0000

    increment verson

--- bin/commander/appinfo.py
+++ bin/commander/appinfo.py
@@ -2,7 +2,7 @@ import os
 hostname = os.uname()[1]
 hostos = os.uname()[2]
 appname = "OpenEmbedded Commander"
-appversion = "0.0.1"
+appversion = "0.0.2"
 appcaption = "%s V%s on %s (%s)" % ( appname, appversion, hostname, hostos )
 imageDir = "%s/bin/commander/images/" % os.environ["OEDIR"]
 
