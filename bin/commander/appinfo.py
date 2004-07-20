7ac6ddf4c4851590fea07992dcddc13bfe3c8f79
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2004-07-20 12:53:21 +0000

    get dir without having to rely on OEDIR

--- bin/commander/appinfo.py
+++ bin/commander/appinfo.py
@@ -4,5 +4,5 @@ hostos = os.uname()[2]
 appname = "OpenEmbedded Commander"
 appversion = "0.0.2"
 appcaption = "%s V%s on %s (%s)" % ( appname, appversion, hostname, hostos )
-imageDir = "%s/bin/commander/images/" % os.environ["OEDIR"]
+imageDir = "%s/images/" % os.path.dirname( __file__ )
 
