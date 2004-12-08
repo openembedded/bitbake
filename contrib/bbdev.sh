9b67e1bd7adb6427ffe3c655f19a0b39c73af3b3
Chris Larson <clarson@kergoth.com>
2004-12-08 09:51:02 +0000

    oe to bb rename in oedev.sh.

--- contrib/bbdev.sh
+++ contrib/bbdev.sh
@@ -4,9 +4,9 @@
 # The author disclaims copyright to the contents of this file and places it in the
 # public domain.
 
-oedev () {
+bbdev () {
 	local BBDIR PKGDIR BUILDDIR
-	if test x"$1" = "x--help"; then echo >&2 "syntax: oedev [oedir [pkgdir [builddir]]]"; return 1; fi
+	if test x"$1" = "x--help"; then echo >&2 "syntax: bbdev [bbdir [pkgdir [builddir]]]"; return 1; fi
 	if test x"$1" = x; then BBDIR=`pwd`; else BBDIR=$1; fi
 	if test x"$2" = x; then PKGDIR=`pwd`; else PKGDIR=$2; fi
 	if test x"$3" = x; then BUILDDIR=`pwd`; else BUILDDIR=$3; fi
@@ -15,7 +15,7 @@ oedev () {
 	PKGDIR=`readlink -f $PKGDIR`
 	BUILDDIR=`readlink -f $BUILDDIR`
 	if ! (test -d $BBDIR && test -d $PKGDIR && test -d $BUILDDIR); then
-		echo >&2 "syntax: oedev [oedir [pkgdir [builddir]]]"
+		echo >&2 "syntax: bbdev [bbdir [pkgdir [builddir]]]"
 		return 1
 	fi
 	
