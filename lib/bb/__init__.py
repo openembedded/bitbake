881272907af4d21bc710e6577eeadc40389c827a
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2005-07-06 08:47:24 +0000

    remove last trace of make.py

--- lib/bb/__init__.py
+++ lib/bb/__init__.py
@@ -20,7 +20,7 @@ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with
 this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place, Suite 330, Boston, MA 02111-1307 USA. 
+Place, Suite 330, Boston, MA 02111-1307 USA.
 """
 
 __version__ = "1.3.0"
@@ -60,7 +60,6 @@ __all__ = [
     "event",
     "build",
     "fetch",
-    "make",
     "manifest"
  ]
 
@@ -298,7 +297,7 @@ def decodeurl(url):
     else:
         user = ''
         pswd = ''
-    
+
     p = {}
     if parm:
         for s in parm.split(';'):
