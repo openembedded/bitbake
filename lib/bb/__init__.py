d8073f1bfe1f259579b7b7400e5ae782187d3253
Holger Hans Peter Freyther <zecke@selfish.org>
2006-02-24 00:11:25 +0000

    lib/bb/__init__.py:     bump the version

--- lib/bb/__init__.py
+++ lib/bb/__init__.py
@@ -23,7 +23,7 @@ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place, Suite 330, Boston, MA 02111-1307 USA.
 """
 
-__version__ = "1.3.3.2"
+__version__ = "1.3.3.3"
 
 __all__ = [
 
