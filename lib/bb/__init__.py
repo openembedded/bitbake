82e5c98315dcb9219faed895cb049605ac7a49b7
Richard Purdie <rpurdie@linux.intel.com>
2006-11-13 00:19:15 +0000

    Bump to version 1.7.4

--- lib/bb/__init__.py
+++ lib/bb/__init__.py
@@ -23,7 +23,7 @@ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place, Suite 330, Boston, MA 02111-1307 USA.
 """
 
-__version__ = "1.7.3"
+__version__ = "1.7.4"
 
 __all__ = [
 
