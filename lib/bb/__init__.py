84187dfabc6b57a047d1436e06d616c816e30555
Richard Purdie <rpurdie@linux.intel.com>
2007-03-04 23:31:05 +0000

    Update versions

--- lib/bb/__init__.py
+++ lib/bb/__init__.py
@@ -21,7 +21,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = "1.7.4"
+__version__ = "1.9.0"
 
 __all__ = [
 
