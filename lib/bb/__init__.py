4eb393be0873f73882d3c48dd80d86aa73cc7b93
Holger Hans Peter Freyther <zecke@selfish.org>
2007-06-30 17:04:34 +0000

    tags/bitbake-1.8.6: Tag BitBake-1.8.6     -Update ChangeLog     -Update version(s) in the tags and in the branch

--- lib/bb/__init__.py
+++ lib/bb/__init__.py
@@ -21,7 +21,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = "1.8.5"
+__version__ = "1.8.7"
 
 __all__ = [
 
