cf07aee98980aaaeb9692e72299c9c699ffd046c
Richard Purdie <rpurdie@linux.intel.com>
2008-01-22 13:09:16 +0000

    git.py: Fix git branch tags fetching

--- lib/bb/fetch/git.py
+++ lib/bb/fetch/git.py
@@ -96,7 +96,7 @@ class Git(Fetch):
         # Remove all but the .git directory
         runfetchcmd("rm * -Rf", d)
         runfetchcmd("git fetch %s://%s%s" % (ud.proto, ud.host, ud.path), d)
-        runfetchcmd("git pull --tags %s://%s%s" % (ud.proto, ud.host, ud.path), d)
+        runfetchcmd("git fetch --tags %s://%s%s" % (ud.proto, ud.host, ud.path), d)
         runfetchcmd("git prune-packed", d)
         runfetchcmd("git pack-redundant --all | xargs -r rm", d)
 
