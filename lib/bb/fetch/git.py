9c3334f8fe28ae97d649a832c5d527347c6d4655
Holger Hans Peter Freyther <zecke@selfish.org>
2008-10-12 22:18:06 +0000

    [git] Disable downloading of checkouts.     Instead of calling try_mirror check if we have a local checkout     already. This code fetched a new checkout even if you had a local     repository which is rather bad if you don't have internet but a full     repository. And the overhead of a full checkout vs. repository is not     too big anyway.

--- lib/bb/fetch/git.py
+++ lib/bb/fetch/git.py
@@ -62,7 +62,7 @@ class Git(Fetch):
     def go(self, loc, ud, d):
         """Fetch url"""
 
-        if Fetch.try_mirror(d, ud.localfile):
+        if os.access(os.path.join(data.getVar("DL_DIR", d, True), ud.localfile), os.R_OK):
             bb.msg.debug(1, bb.msg.domain.Fetcher, "%s already exists (or was stashed). Skipping git checkout." % ud.localpath)
             return
 
