eeca44bb6767e852c129bf61c4cfd776537ec19b
Richard Purdie <rpurdie@linux.intel.com>
2008-12-06 13:04:01 +0000

    shell.py: Catch up with core changes

--- lib/bb/shell.py
+++ lib/bb/shell.py
@@ -151,9 +151,6 @@ class BitBakeShellCommands:
         if len( names ) == 0: names = [ globexpr ]
         print "SHELL: Building %s" % ' '.join( names )
 
-        oldcmd = cooker.configuration.cmd
-        cooker.configuration.cmd = cmd
-
         td = taskdata.TaskData(cooker.configuration.abort)
         localdata = data.createCopy(cooker.configuration.data)
         data.update_data(localdata)
@@ -168,7 +165,7 @@ class BitBakeShellCommands:
                 if len(providers) == 0:
                     raise Providers.NoProvider
 
-                tasks.append([name, "do_%s" % cooker.configuration.cmd])
+                tasks.append([name, "do_%s" % cmd])
 
             td.add_unresolved(localdata, cooker.status)
             
@@ -189,7 +186,6 @@ class BitBakeShellCommands:
             print "ERROR: Couldn't build '%s'" % names
             last_exception = e
 
-        cooker.configuration.cmd = oldcmd
 
     build.usage = "<providee>"
 
