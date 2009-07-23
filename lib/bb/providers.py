72bf7476b2492b6524cb8ff87ee5f4e86b28d975
Richard Purdie <rpurdie@linux.intel.com>
2009-07-23 17:48:18 +0100

    providers.py: Add caching for the dynamic package regexps (from Poky)
    Add caching for the dyanmic package regexps which improves task data
    performance significantly.
    
    Signed-off-by: Richard Purdie <rpurdie@linux.intel.com>

--- lib/bb/providers.py
+++ lib/bb/providers.py
@@ -280,6 +280,8 @@ def filterProvidersRunTime(providers, item, cfgData, dataCache):
 
     return eligible, numberPreferred
 
+regexp_cache = {}
+
 def getRuntimeProviders(dataCache, rdepend):
     """
     Return any providers of runtime dependency
@@ -297,11 +299,16 @@ def getRuntimeProviders(dataCache, rdepend):
 
     # Only search dynamic packages if we can't find anything in other variables
     for pattern in dataCache.packages_dynamic:
-        try:
-            regexp = re.compile(pattern.replace('+', "\+"))
-        except:
-            bb.msg.error(bb.msg.domain.Provider, "Error parsing re expression: %s" % pattern)
-            raise
+        pattern = pattern.replace('+', "\+")
+        if pattern in regexp_cache:
+            regexp = regexp_cache[pattern]
+        else:
+            try:
+                regexp = re.compile(pattern)
+            except:
+                bb.msg.error(bb.msg.domain.Provider, "Error parsing re expression: %s" % pattern)
+                raise
+            regexp_cache[pattern] = regexp
         if regexp.match(rdepend):
             rproviders += dataCache.packages_dynamic[pattern]
 
