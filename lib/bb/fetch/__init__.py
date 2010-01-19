c8aa1c378b7473c82a1ee43f4cecfa65eb44bf7b
Richard Purdie <rpurdie@linux.intel.com>
2010-01-19 14:49:50 +0000

    fetch: Add GIT_PROXY_COMMAND to runfetchcmd function
    Signed-off-by: Richard Purdie <rpurdie@linux.intel.com>

--- lib/bb/fetch/__init__.py
+++ lib/bb/fetch/__init__.py
@@ -300,7 +300,7 @@ def runfetchcmd(cmd, d, quiet = False):
     # rather than host provided
     # Also include some other variables.
     # FIXME: Should really include all export varaiables?
-    exportvars = ['PATH', 'GIT_PROXY_HOST', 'GIT_PROXY_PORT', 'GIT_CONFIG', 'http_proxy', 'ftp_proxy', 'SSH_AUTH_SOCK', 'SSH_AGENT_PID', 'HOME']
+    exportvars = ['PATH', 'GIT_PROXY_COMMAND', 'GIT_PROXY_HOST', 'GIT_PROXY_PORT', 'GIT_CONFIG', 'http_proxy', 'ftp_proxy', 'SSH_AUTH_SOCK', 'SSH_AGENT_PID', 'HOME']
 
     for var in exportvars:
         val = data.getVar(var, d, True)
