d6ff58eb2d2a708bdd7961a50a877101ce420e59
Richard Purdie <rpurdie@linux.intel.com>
2010-07-25 11:32:42 +0100

    fetch: Only mark srcrev recipes as uncached when outside of SRCREV caching mode
    (From Poky rev: 36cfff8ed5b8a777a09cc8abcd8b01a1f297e404)
    
    Signed-off-by: Richard Purdie <rpurdie@linux.intel.com>

--- lib/bb/fetch/__init__.py
+++ lib/bb/fetch/__init__.py
@@ -355,7 +355,8 @@ def get_srcrev(d):
         bb.msg.error(bb.msg.domain.Fetcher, "SRCREV was used yet no valid SCM was found in SRC_URI")
         raise ParameterError
 
-    bb.data.setVar('__BB_DONT_CACHE', '1', d)
+    if bb.data.getVar('BB_SRCREV_POLICY', d, True) != "cache":
+        bb.data.setVar('__BB_DONT_CACHE', '1', d)
 
     if len(scms) == 1:
         return urldata[scms[0]].method.sortable_revision(scms[0], urldata[scms[0]], d)
