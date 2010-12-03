ostroproject/ostro-os

bitbake/build.py: Note when calling functions that don't exist. Should probably error
(From OE-core rev: 9ef9b5e1a7e74aec9bc68f5a8e18354247bd90fe)

Signed-off-by: Richard Purdie <rpurdie@linux.intel.com>

bitbake/lib/bb/build.py
@@ -90,6 +90,7 @@ def exec_func(func, d, dirs = None):
 
     body = data.getVar(func, d)
     if not body:
+        bb.warn("Function %s doesn't exist" % func)
         return
 
     flags = data.getVarFlags(func, d)

openembedded/bitbake

Signed-off-by: Chris Larson <chris_larson@mentor.com>

lib/bb/build.py
@@ -104,6 +104,7 @@ def exec_func(func, d, dirs = None):
 
     body = data.getVar(func, d)
     if not body:
+        logger.warn("Function %s doesn't exist" % func)
         return
 
     flags = data.getVarFlags(func, d)