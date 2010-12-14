42b08455b9cc6a48e1c563084c9698542a5c6c96
Kevin Tian <kevin.tian@intel.com>
2010-12-14 19:21:24 +0800

    runqueue: use correct task ID when checking validity of setscene tasks
    sstate hash validation is done at initialization of RunQueueExecuteScenequeue.
    However the index of 'valid' list returned from the validation doesn't
    correspond to setscene task ID. It's just an intermediate namespace between
    runqueue and sstate hash func. Use it as setscene task ID fully mess the flow.
    Previously this doesn't cause trouble because all setscene tasks are passed. Commit
    58396a5d24c62710fd0a9f3780d84ac8a95d8e7c add 'noexec' concept to setscene
    tasks which grabs some tasks out of the list and thus trigger this problem
    
    Without this fix there're ~50 recipes (gzip-native, glib, ...) rebuilt weirdly
    with a minimal build, even though existing sstate packages could accelerate them.
    
    there's another typo using wrong task ID in a debug message which further hide
    this issue
    
    Signed-off-by: Kevin Tian <kevin.tian@intel.com>

--- lib/bb/runqueue.py
+++ lib/bb/runqueue.py
@@ -1367,6 +1367,7 @@ class RunQueueExecuteScenequeue(RunQueueExecute):
             sq_hash = []
             sq_hashfn = []
             sq_fn = []
+            sq_taskname = []
             sq_task = []
             noexec = []
             for task in range(len(self.sq_revdeps)):
@@ -1382,13 +1383,18 @@ class RunQueueExecuteScenequeue(RunQueueExecute):
                 sq_fn.append(fn)
                 sq_hashfn.append(self.rqdata.dataCache.hashfn[fn])
                 sq_hash.append(self.rqdata.runq_hash[realtask])
-                sq_task.append(taskname)
-
+                sq_taskname.append(taskname)
+                sq_task.append(task)
             call = self.rq.hashvalidate + "(sq_fn, sq_task, sq_hash, sq_hashfn, d)"
-            locs = { "sq_fn" : sq_fn, "sq_task" : sq_task, "sq_hash" : sq_hash, "sq_hashfn" : sq_hashfn, "d" : self.cooker.configuration.data }
+            locs = { "sq_fn" : sq_fn, "sq_task" : sq_taskname, "sq_hash" : sq_hash, "sq_hashfn" : sq_hashfn, "d" : self.cooker.configuration.data }
             valid = bb.utils.better_eval(call, locs)
+
+            valid_new = []
+            for v in valid:
+                valid_new.append(sq_task[v])
+
             for task in range(len(self.sq_revdeps)):
-                if task not in valid and task not in noexec:
+                if task not in valid_new and task not in noexec:
                     logger.debug(2, 'No package found, so skipping setscene task %s',
                                  self.rqdata.get_user_idstring(task))
                     self.task_failoutright(task)
