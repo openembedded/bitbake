0ea334430d492f82896c8b114dd3da214a37ae99
Chris Larson <chris_larson@mentor.com>
2010-12-16 07:51:53 -0700

    Revert "build: kill stdout in python functions"
    We have a better solution for this on the separate-ui-and-server branch, so
    this version is no longer necessary.
    
    This reverts commit 20929afdd87e5124891f121c3640aa9efa368c2c.
    
    Signed-off-by: Chris Larson <chris_larson@mentor.com>

--- lib/bb/build.py
+++ lib/bb/build.py
@@ -187,9 +187,6 @@ def exec_func_python(func, d, runfile, logfile, cwd=None):
     if cwd:
         os.chdir(cwd)
 
-    stdout, stderr = sys.stdout, sys.stderr
-    sys.stdout, sys.stderr = NULL, NULL
-
     handler = logging.StreamHandler(logfile)
     handler.setFormatter(logformatter)
     bblogger.addHandler(handler)
@@ -204,7 +201,6 @@ def exec_func_python(func, d, runfile, logfile, cwd=None):
         raise FuncFailed(func, None)
     finally:
         bblogger.removeHandler(handler)
-        sys.stdout, sys.stderr = stdout, stderr
         os.chdir(olddir)
 
 def exec_func_shell(function, d, runfile, logfile, cwd=None, fakeroot=False):
