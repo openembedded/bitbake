4d9e3f12a2eac8fe9999e6822f0dcd8498d99330
Chris Larson <chris_larson@mentor.com>
2010-12-29 17:52:54 -0700

    utils: kill unnecessary duplicated chmod
    Signed-off-by: Chris Larson <chris_larson@mentor.com>

--- lib/bb/utils.py
+++ lib/bb/utils.py
@@ -739,8 +739,6 @@ def copyfile(src, dest, newmtime = None, sstat = None):
             os.rename(dest + "#new", dest)
         except Exception as e:
             print('copyfile: copy', src, '->', dest, 'failed.', e)
-            os.chmod(src, stat.S_IMODE(sstat[stat.ST_MODE]))
-            os.utime(src, (sstat[stat.ST_ATIME], sstat[stat.ST_MTIME]))
             return False
         finally:
             os.chmod(src, sstat[stat.ST_MODE])
