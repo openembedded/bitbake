9e9c75761e51c852e029ccd638d9df35d04129da
Chris Larson <chris_larson@mentor.com>
2011-03-18 09:56:50 -0700

    build: add missing bit necessary for mhatle's fakeroot fix
    Signed-off-by: Chris Larson <chris_larson@mentor.com>

--- lib/bb/build.py
+++ lib/bb/build.py
@@ -442,6 +442,7 @@ def add_tasks(tasklist, d):
         getTask('rdeptask')
         getTask('recrdeptask')
         getTask('nostamp')
+        getTask('fakeroot')
         getTask('noexec')
         task_deps['parents'][task] = []
         for dep in flags['deps']:
