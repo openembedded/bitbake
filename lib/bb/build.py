openembedded/bitbake

commit 7a29ab534388c0095f7f826b16c5cff343927d10

build: fix dir removal traceback
This one is to cover the case where the current directory vanishes out from
under us, so os.getcwd() raises an OSError.

Signed-off-by: Chris Larson <chris_larson@mentor.com>

lib/bb/build.py
@@ -181,7 +181,6 @@ def exec_func_python(func, d, runfile, cwd=None):
     """Execute a python BB 'function'"""
 
     bbfile = d.getVar('FILE', True)
-    olddir = os.getcwd()
     code = _functionfmt.format(function=func, body=d.getVar(func, True))
     bb.utils.mkdirhier(os.path.dirname(runfile))
     with open(runfile, 'w') as script:
@@ -189,6 +188,10 @@ def exec_func_python(func, d, runfile, cwd=None):
 
     if cwd:
         os.chdir(cwd)
+        try:
+            olddir = os.getcwd()
+        except OSError:
+            olddir = None
 
     try:
         comp = utils.better_compile(code, func, bbfile)
@@ -199,7 +202,8 @@ def exec_func_python(func, d, runfile, cwd=None):
 
         raise FuncFailed(func, None)
     finally:
-        os.chdir(olddir)
+        if cwd and olddir:
+            os.chdir(olddir)
 
 def exec_func_shell(function, d, runfile, cwd=None, fakeroot=False):
     """Execute a shell function from the metadata

commit 933350f801d95762cba3a1f260404fd25fd0a73c

build.py: Fix ordering bug introduced in 7a29ab534388c0095f7f826b16c5cff343927d10
Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

lib/bb/build.py
@@ -187,11 +187,11 @@ def exec_func_python(func, d, runfile, cwd=None):
         script.write(code)
 
     if cwd:
-        os.chdir(cwd)
         try:
             olddir = os.getcwd()
         except OSError:
             olddir = None
+        os.chdir(cwd)
 
     try:
         comp = utils.better_compile(code, func, bbfile)