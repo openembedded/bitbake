97e8adf03e5fab1fd40c3d53c48f7b333bc2e145
Richard Purdie <richard.purdie@linuxfoundation.org>
2011-05-27 16:13:27 +0100

    bitbake/utils.py: Add option to lockfiles to return immediately rather than wait
    There are usecases where we don't want to block waiting for a lockfile
    so enhance the lockfile handling functions to support this.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/bb/utils.py
+++ lib/bb/utils.py
@@ -402,7 +402,7 @@ def fileslocked(files):
     for lock in locks:
         bb.utils.unlockfile(lock)
 
-def lockfile(name, shared=False):
+def lockfile(name, shared=False, retry=True):
     """
     Use the file fn as a lock file, return when the lock has been acquired.
     Returns a variable to pass to unlockfile().
@@ -418,6 +418,8 @@ def lockfile(name, shared=False):
     op = fcntl.LOCK_EX
     if shared:
         op = fcntl.LOCK_SH
+    if not retry:
+        op = op | fcntl.LOCK_NB
 
     while True:
         # If we leave the lockfiles lying around there is no problem
@@ -442,6 +444,8 @@ def lockfile(name, shared=False):
             lf.close()
         except Exception:
             continue
+        if not retry:
+            return None
 
 def unlockfile(lf):
     """
