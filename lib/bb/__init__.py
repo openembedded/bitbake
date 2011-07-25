f0b5d16426b983a67c51c47f3542162108bd4156
Richard Purdie <richard.purdie@linuxfoundation.org>
2011-07-25 13:50:38 +0100

    Update version to 1.13.3
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/bb/__init__.py
+++ lib/bb/__init__.py
@@ -21,7 +21,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = "1.13.2"
+__version__ = "1.13.3"
 
 import sys
 if sys.version_info < (2, 6, 0):
