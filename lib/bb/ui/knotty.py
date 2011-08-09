4d4e01aa3e38d809617e8c1bbaa081182a2308a0
Richard Purdie <richard.purdie@linuxfoundation.org>
2011-08-09 12:29:17 +0100

    bitbake/ui/knotty: Ensure 'No Provider' errors set an exit code
    If a No Provider error is shown we need to set the exit code to
    show an error occured too.
    
    [YOCTO #1322]
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/bb/ui/knotty.py
+++ lib/bb/ui/knotty.py
@@ -199,6 +199,7 @@ def main(server, eventHandler):
                 logger.info("consider defining a PREFERRED_PROVIDER entry to match %s", event._item)
                 continue
             if isinstance(event, bb.event.NoProvider):
+                return_value = 1
                 if event._runtime:
                     r = "R"
                 else:
