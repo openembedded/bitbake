6ed1a28eb0e52a6de83a37664ff6f4418ce84ee4
Alexandru DAMIAN <alexandru.damian@intel.com>
2014-01-13 17:03:41 +0000

    toaster: update Simple UI references to avoid conflict
    Since the Toaster GUI was cloned from the Simple UI, we need
    to update the URL pattern names in Simple UI to
    prevent conflict when determining the reverse URL path.
    
    Signed-off-by: Alexandru DAMIAN <alexandru.damian@intel.com>

--- lib/toaster/bldviewer/urls.py
+++ lib/toaster/bldviewer/urls.py
@@ -20,13 +20,13 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import RedirectView
 
 urlpatterns = patterns('bldviewer.views',
-        url(r'^builds/$', 'build', name='all-builds'),
-        url(r'^build/(?P<build_id>\d+)/task/$', 'task', name='task'),
-        url(r'^build/(?P<build_id>\d+)/packages/$', 'bpackage', name='bpackage'),
-        url(r'^build/(?P<build_id>\d+)/package/(?P<package_id>\d+)/files/$', 'bfile', name='bfile'),
-        url(r'^build/(?P<build_id>\d+)/target/(?P<target_id>\d+)/packages/$', 'tpackage', name='tpackage'),
-        url(r'^build/(?P<build_id>\d+)/configuration/$', 'configuration', name='configuration'),
-        url(r'^layers/$', 'layer', name='all-layers'),
-        url(r'^layerversions/(?P<layerversion_id>\d+)/recipes/.*$', 'layer_versions_recipes', name='layer_versions_recipes'),
+        url(r'^builds/$', 'build', name='simple-all-builds'),
+        url(r'^build/(?P<build_id>\d+)/task/$', 'task', name='simple-task'),
+        url(r'^build/(?P<build_id>\d+)/packages/$', 'bpackage', name='simple-bpackage'),
+        url(r'^build/(?P<build_id>\d+)/package/(?P<package_id>\d+)/files/$', 'bfile', name='simple-bfile'),
+        url(r'^build/(?P<build_id>\d+)/target/(?P<target_id>\d+)/packages/$', 'tpackage', name='simple-tpackage'),
+        url(r'^build/(?P<build_id>\d+)/configuration/$', 'configuration', name='simple-configuration'),
+        url(r'^layers/$', 'layer', name='simple-all-layers'),
+        url(r'^layerversions/(?P<layerversion_id>\d+)/recipes/.*$', 'layer_versions_recipes', name='simple-layer_versions_recipes'),
         url(r'^$', RedirectView.as_view( url= 'builds/')),
 )
