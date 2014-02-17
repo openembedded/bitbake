cf90bd6b2a0ab7dce922bffb500d6a2ff2ff10e2
Paul Eggleton <paul.eggleton@linux.intel.com>
2014-02-17 14:07:41 +0000

    build: fix handling of task override for tasks with underscores in their names
    Tasks whose names contain underscores (such as do_populate_sdk in OE)
    when converted to a task override do not function properly. If we
    replace underscores with hyphens we can still have a working override
    for these tasks.
    
    Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/bb/build.py
+++ lib/bb/build.py
@@ -330,7 +330,7 @@ def _task_data(fn, task, d):
     localdata.setVar('BB_FILENAME', fn)
     localdata.setVar('BB_CURRENTTASK', task[3:])
     localdata.setVar('OVERRIDES', 'task-%s:%s' %
-                     (task[3:], d.getVar('OVERRIDES', False)))
+                     (task[3:].replace('_', '-'), d.getVar('OVERRIDES', False)))
     localdata.finalize()
     bb.data.expandKeys(localdata)
     return localdata
