e24095f54c52a547c0462836586a5d716249036e
Richard Purdie <richard.purdie@linuxfoundation.org>
2014-09-11 17:31:09 +0100

    bin/bitbake: Update to version 1.23.2
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/bb/__init__.py
+++ lib/bb/__init__.py
@@ -21,7 +21,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = "1.23.1"
+__version__ = "1.23.2"
 
 import sys
 if sys.version_info < (2, 7, 3):
