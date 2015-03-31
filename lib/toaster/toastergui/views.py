451e85fe8592cab0a07a6866b2853a29cc38f14b
Alexandru DAMIAN <alexandru.damian@intel.com>
2015-03-31 13:57:40 +0100

    toastergui: add ext4 option for IMAGE_FSTYPES
    This patch adds "ext4" as a selectable option in IMAGE_FSTYPES,
    to keep in sync with the OE-Core capabilities.
    
    Signed-off-by: Alexandru DAMIAN <alexandru.damian@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/toaster/toastergui/views.py
+++ lib/toaster/toastergui/views.py
@@ -3045,7 +3045,7 @@ if toastermain.settings.MANAGED:
 
         vars_fstypes  = {
             'btrfs','cpio','cpio.gz','cpio.lz4','cpio.lzma','cpio.xz','cramfs',
-            'elf','ext2','ext2.bz2','ext2.gz','ext2.lzma', 'ext3','ext3.gz','hddimg',
+            'elf','ext2','ext2.bz2','ext2.gz','ext2.lzma', 'ext4', 'ext4.gz', 'ext3','ext3.gz','hddimg',
             'iso','jffs2','jffs2.sum','squashfs','squashfs-lzo','squashfs-xz','tar.bz2',
             'tar.lz4','tar.xz','tartar.gz','ubi','ubifs','vmdk'
         }
