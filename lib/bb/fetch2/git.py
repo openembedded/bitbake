93e4c9bb2393b1074f5a01e7eaaac742a59d8086
Romain Perier <romain.perier@gmail.com>
2015-09-18 18:35:22 +0200

    bitbake: bb.fetch2.git: Import errno module
    Currently this module is dereferencing errno.ENOENT but the python module "errno"
    is not imported, which causes a crash when fetching from a git repository.
    
    Signed-off-by: Romain Perier <romain.perier@gmail.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/bb/fetch2/git.py
+++ lib/bb/fetch2/git.py
@@ -66,6 +66,7 @@ Supported SRC_URI options are:
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+import errno
 import os
 import re
 import bb
