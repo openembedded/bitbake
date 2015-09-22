aab4aff75eefb31aa53885d7735feee5daa294aa
Ed Bartosh <ed.bartosh@linux.intel.com>
2015-09-22 10:34:53 +0100

    toaster: don't re-create Target objects
    Due to re-creating Target objects from bitbake events task information
    stored in original objects is lost.
    
    There is no valid reason to remove existing objects. It's safer to query
    them instead of re-creating as original object contain more information
    than events coming from bitbake.
    
    Signed-off-by: Ed Bartosh <ed.bartosh@linux.intel.com>
    Signed-off-by: Elliot Smith <elliot.smith@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/bb/ui/buildinfohelper.py
+++ lib/bb/ui/buildinfohelper.py
@@ -161,8 +161,6 @@ class ORMWrapper(object):
             build.bitbake_version=build_info['bitbake_version']
             build.save()
 
-            Target.objects.filter(build = build).delete()
-
         else:
             build = Build.objects.create(
                                     project = prj,
@@ -182,19 +180,6 @@ class ORMWrapper(object):
             buildrequest.save()
 
         return build
-
-    def create_target_objects(self, target_info):
-        assert 'build' in target_info
-        assert 'targets' in target_info
-
-        targets = []
-        for tgt_name in target_info['targets']:
-            tgt_object = Target.objects.create( build = target_info['build'],
-                                    target = tgt_name,
-                                    is_image = False,
-                                    )
-            targets.append(tgt_object)
-        return targets
 
     def update_build_object(self, build, errors, warnings, taskfailures):
         assert isinstance(build,Build)
@@ -869,7 +854,7 @@ class BuildInfoHelper(object):
         target_information['targets'] = event._pkgs
         target_information['build'] = build_obj
 
-        self.internal_state['targets'] = self.orm_wrapper.create_target_objects(target_information)
+        self.internal_state['targets'] = Target.objects.filter(build=target_information['build'])
 
         # Save build configuration
         data = self.server.runCommand(["getAllKeysWithFlags", ["doc", "func"]])[0]
