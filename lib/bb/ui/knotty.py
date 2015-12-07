67b77658e2bfa849f6f55c9c262cb11d6bfdb399
Richard Purdie <richard.purdie@linuxfoundation.org>
2015-12-07 18:26:57 +0000

    knotty: Enforce terminal line limit to stop crazy scrolling
    If there are more tasks running than there are lines on the terminal, the data
    scrolls in ways the UI wasn't designed for. This patch adjusts the UI just to
    show the processes which fit onto the number of rows in the terminal window.
    You can see the total number running from the counter in the top left as usual
    and this makes warning and errors messages scrolling from the top of the window
    work as designed.
    
    Ultimately, scrolling would be nice but is for another time, this fixes the
    biggest UI issue on highly parallel machines.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/bb/ui/knotty.py
+++ lib/bb/ui/knotty.py
@@ -104,10 +104,11 @@ class InteractConsoleLogFilter(logging.Filter):
         return True
 
 class TerminalFilter(object):
+    rows = 25
     columns = 80
 
     def sigwinch_handle(self, signum, frame):
-        self.columns = self.getTerminalColumns()
+        self.rows, self.columns = self.getTerminalColumns()
         if self._sigwinch_default:
             self._sigwinch_default(signum, frame)
 
@@ -131,7 +132,7 @@ class TerminalFilter(object):
                 cr = (env['LINES'], env['COLUMNS'])
             except:
                 cr = (25, 80)
-        return cr[1]
+        return cr
 
     def __init__(self, main, helper, console, errconsole, format):
         self.main = main
@@ -207,7 +208,7 @@ class TerminalFilter(object):
             content = "Currently %s running tasks (%s of %s):" % (len(activetasks), self.helper.tasknumber_current, self.helper.tasknumber_total)
         print(content)
         lines = 1 + int(len(content) / (self.columns + 1))
-        for tasknum, task in enumerate(tasks):
+        for tasknum, task in enumerate(tasks[:(self.rows - 2)]):
             content = "%s: %s" % (tasknum, task)
             print(content)
             lines = lines + 1 + int(len(content) / (self.columns + 1))
