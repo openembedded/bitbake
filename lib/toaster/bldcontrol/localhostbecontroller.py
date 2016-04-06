062c68e3e7d00834118fe07ceb0899874be714ae
Ed Bartosh <ed.bartosh@linux.intel.com>
2016-04-06 17:46:24 +0100

    toaster: stop bitbake server after the build
    Bitbake server is used only during the build. There is no need
    in keeping server running after the build.
    
    Running bitbake -m in the subshell after the build should stop
    the server.
    
    Signed-off-by: Ed Bartosh <ed.bartosh@linux.intel.com>
    Signed-off-by: Michael Wood <michael.g.wood@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/toaster/bldcontrol/localhostbecontroller.py
+++ lib/toaster/bldcontrol/localhostbecontroller.py
@@ -317,11 +317,13 @@ class LocalhostBEController(BuildEnvironmentController):
                 task = ':%s' % task
             bbtargets += '%s%s ' % (target.target, task)
 
-        # run build with local bitbake
+        # run build with local bitbake. stop the server after the build.
         log = os.path.join(builddir, 'toaster_ui.log')
-        self._shellcmd('TOASTER_BRBE="%s" BBSERVER="0.0.0.0:-1" '
-                       '../bitbake/bin/bitbake %s -u toasterui '
-                       '>>%s 2>&1 &' % (brbe, bbtargets, log), builddir)
+        self._shellcmd(['(TOASTER_BRBE="%s" BBSERVER="0.0.0.0:-1" '
+                        '../bitbake/bin/bitbake %s -u toasterui >>%s 2>&1;'
+                        'BITBAKE_UI="" BBSERVER=0.0.0.0:-1 %s -m)&' \
+                        % (brbe, bbtargets, log, bitbake)], builddir,
+                        nowait=True)
 
         logger.debug('localhostbecontroller: Build launched, exiting. '
                      'Follow build logs at %s' % log)
