f08651a954358e9c6ce4d69de285e4cd9e1408ae
Michael Wood <michael.g.wood@intel.com>
2016-04-26 17:18:07 +0100

    toaster: customrecipe Only show download icon or button if it's possible
    If the based on recipe has not yet been checked out/cloned we cannot
    generate the custom image recipe file that uses it. So disable/remove
    the option to download it.
    
    [YOCTO #9425]
    
    Signed-off-by: Michael Wood <michael.g.wood@intel.com>
    Signed-off-by: Elliot Smith <elliot.smith@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/toaster/toastergui/tables.py
+++ lib/toaster/toastergui/tables.py
@@ -508,10 +508,12 @@ class CustomImagesTable(ToasterTable):
 
         recipe_file_template = '''
         <code>{{data.name}}_{{data.version}}.bb</code>
+        {% if data.get_base_recipe_file %}
         <a href="{% url 'customrecipedownload' extra.pid data.pk %}">
         <i class="icon-download-alt" data-original-title="Download recipe
         file"></i>
-        </a>'''
+        </a>
+        {% endif %}'''
 
         self.add_column(title="Recipe file",
                         static_data_name='recipe_file_download',
