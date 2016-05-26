cfd481fe9799e7a4c6bfac32e56cc91cfcd81088
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2016-05-26 02:08:06 +0200

    fetch: copy files with -H
    When using a PREMIRROR with plain (non-unpack) files, a SRC_URI like
    
    SRC_URI = "file://devmem2.c"
    
    will cause devmem2.c to be a symlink in the WORKDIR pointing to the
    local PREMIRROR.
    
    Trying to apply a patch on this file will either modify the file on
    the PREMIRROR or will fail due to sanity checks:
    
    ERROR: devmem2-1.0-r7 do_patch: Command Error: 'quilt --quiltrc /cache/build-ubuntu/sysroots/x86_64-oe-linux/etc/quiltrc push' exited with 1  Output:
    Applying patch devmem2-fixups-2.patch
    File devmem2.c is not a regular file -- refusing to patch
    
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/bb/fetch2/__init__.py
+++ lib/bb/fetch2/__init__.py
@@ -1431,7 +1431,7 @@ class FetchMethod(object):
                     if urlpath.find("/") != -1:
                         destdir = urlpath.rsplit("/", 1)[0] + '/'
                         bb.utils.mkdirhier("%s/%s" % (unpackdir, destdir))
-                cmd = 'cp -fpPR %s %s' % (file, destdir)
+                cmd = 'cp -fpPRH %s %s' % (file, destdir)
 
         if not cmd:
             return
