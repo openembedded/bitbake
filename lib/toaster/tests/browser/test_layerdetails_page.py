34943b2278efe99c6744399e04a47cdda630468e
Elliot Smith <elliot.smith@intel.com>
2016-07-15 12:09:57 +0100

    toaster-tests: fix erroneous message when test fails
    When one of the layer details tests fails (as it occasionally
    does, if running on a machine under heavy load, due to sync issues),
    the error message shown is misleading, as it is something like:
    
    "Expected 'This was imported' in ['This was imported', ...]"
    
    The string 'This was imported' is in the list shown in the message,
    but the message suggests it isn't.
    
    This is because the test compares the string with one list, but
    then uses a different list in the fail message if the comparison
    fails.
    
    Fix the list shown in the message about the test failing.
    
    Signed-off-by: Elliot Smith <elliot.smith@intel.com>

--- lib/toaster/tests/browser/test_layerdetails_page.py
+++ lib/toaster/tests/browser/test_layerdetails_page.py
@@ -113,8 +113,8 @@ class TestLayerDetailsPage(SeleniumTestCase):
         new_values = ["%s-edited" % old_val
                       for old_val in self.initial_values]
 
-        for inputs in self.find_all("dd input[type=text]") + \
-                self.find_all("dd textarea"):
+        for inputs in self.find_all('dd input[type="text"]') + \
+                self.find_all('dd textarea'):
             # ignore the tt inputs (twitter typeahead input)
             if "tt-" in inputs.get_attribute("class"):
                 continue
@@ -122,8 +122,8 @@ class TestLayerDetailsPage(SeleniumTestCase):
             value = inputs.get_attribute("value")
 
             self.assertTrue(value in new_values,
-                            "Expecting any of \"%s\"but got \"%s\"" %
-                            (self.initial_values, value))
+                            "Expecting any of \"%s\" but got \"%s\"" %
+                            (new_values, value))
 
     def test_delete_layer(self):
         """ Delete the layer """
