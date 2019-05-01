34ed28a412af642a993642c14bd8b95d5ef22cd8
Richard Purdie <richard.purdie@linuxfoundation.org>
2019-05-01 11:47:13 +0100

    bitbake: Drop duplicate license boilerplace text
    With the introduction of SPDX-License-Identifier headers, we don't need a ton
    of header boilerplate in every file. Simplify the files and rely on the top
    level for the full licence text.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>
    Signed-off-by: Armin Kuster <akuster808@gmail.com>

--- lib/bb/fetch2/s3.py
+++ lib/bb/fetch2/s3.py
@@ -15,19 +15,6 @@ The aws tool must be correctly installed and configured prior to use.
 #
 # SPDX-License-Identifier: GPL-2.0-only
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
 # Based on functions from the base bb module, Copyright 2003 Holger Schurig
 
 import os
