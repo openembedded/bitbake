1412dcc077b1bea10b72fc8b525d6258dca46d97
Joshua Watt <jpewhacker@gmail.com>
2019-12-06 12:12:56 -0600

    Bump minimum python version to 3.5
    The local hash equivalence server used by bitbake requires python 3.5,
    so bump up the minimum required version.
    
    [YOCTO #13678]
    
    Signed-off-by: Joshua Watt <JPEWhacker@gmail.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/bb/__init__.py
+++ lib/bb/__init__.py
@@ -12,8 +12,8 @@
 __version__ = "1.44.0"
 
 import sys
-if sys.version_info < (3, 4, 0):
-    raise RuntimeError("Sorry, python 3.4.0 or later is required for this version of bitbake")
+if sys.version_info < (3, 5, 0):
+    raise RuntimeError("Sorry, python 3.5.0 or later is required for this version of bitbake")
 
 
 class BBHandledException(Exception):
