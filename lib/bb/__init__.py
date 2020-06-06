5f363e4a9636b902229c257484ae0b479aedca65
Joshua Watt <JPEWhacker@gmail.com>
2020-06-05 22:15:32 -0500

    bitbake: lib: Add PrefixLoggerAdapter helper
    Adds a helper logger adapter to add a prefix to all log messages. This
    is useful to distinguish log messages between multiple instances of a
    object.
    
    Signed-off-by: Joshua Watt <JPEWhacker@gmail.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/bb/__init__.py
+++ lib/bb/__init__.py
@@ -104,6 +104,14 @@ logger.setLevel(logging.DEBUG - 2)
 
 mainlogger = logging.getLogger("BitBake.Main")
 
+class PrefixLoggerAdapter(logging.LoggerAdapter):
+    def __init__(self, prefix, logger):
+        super().__init__(logger, {})
+        self.__msg_prefix = prefix
+
+    def process(self, msg, kwargs):
+        return "%s%s" %(self.__msg_prefix, msg), kwargs
+
 # This has to be imported after the setLoggerClass, as the import of bb.msg
 # can result in construction of the various loggers.
 import bb.msg
