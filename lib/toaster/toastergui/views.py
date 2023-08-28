4f5b1f5bede402295bf4dfc8845fe2f38973e157
David Reyna <David.Reyna@windriver.com>
2023-08-28 01:45:18 -0700

    toaster: Update to Django 4.2
    Update Toaster to support Django 4.2, to match current
    hosts and to address CVEs.
    
    [YOCTO #15152]
    
    Signed-off-by: Kieran McNulty <Kieran.McNulty@windriver.com>
    Signed-off-by: David Reyna <David.Reyna@windriver.com>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- lib/toaster/toastergui/views.py
+++ lib/toaster/toastergui/views.py
@@ -670,11 +670,11 @@ def xhr_dirinfo(request, build_id, target_id):
     return HttpResponse(_get_dir_entries(build_id, target_id, top), content_type = "application/json")
 
 from django.utils.functional import Promise
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str
 class LazyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Promise):
-            return force_text(obj)
+            return force_str(obj)
         return super(LazyEncoder, self).default(obj)
 
 from toastergui.templatetags.projecttags import filtered_filesizeformat
