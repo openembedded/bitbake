dfc15ef99302dea22a051c9eb8398ffd5cf1fc20
Richard Purdie <richard.purdie@linuxfoundation.org>
2024-06-04 15:25:35 +0100

    runqueue: Avoid save_unitaskhashes
    The save comes with an IO overhead which can slow down the rehash loop in bitbake
    a lot. We only needed to do this when recipes were doing unihash cache copying. Now
    they aren't doing that, drop this IO pain point.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/bb/runqueue.py
+++ lib/bb/runqueue.py
@@ -2550,9 +2550,6 @@ class RunQueueExecute:
                     self.rqdata.runtaskentries[hashtid].unihash = unihash
                     bb.parse.siggen.set_unihash(hashtid, unihash)
                     toprocess.add(hashtid)
-                if torehash:
-                    # Need to save after set_unihash above
-                    bb.parse.siggen.save_unitaskhashes()
 
         # Work out all tasks which depend upon these
         total = set()
