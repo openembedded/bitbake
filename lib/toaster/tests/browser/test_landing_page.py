3be5e7c78129abf059476416279718156fc666b5
Richard Purdie <richard.purdie@linuxfoundation.org>
2024-10-17 23:22:33 +0100

    toaster/tests/browser: Drop unneeded poll parameters
    In my testing I wasn't able to see issues needing the poll parameter in the
    browser tests. Drop those.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/toaster/tests/browser/test_landing_page.py
+++ lib/toaster/tests/browser/test_landing_page.py
@@ -224,7 +224,7 @@ class TestLandingPage(SeleniumTestCase):
 
         self.get(reverse('landing'))
 
-        self.wait_until_visible("#latest-builds", poll=3)
+        self.wait_until_visible("#latest-builds")
         elements = self.find_all('#allbuildstable')
         self.assertEqual(len(elements), 1, 'should redirect to builds')
         content = self.get_page_source()
