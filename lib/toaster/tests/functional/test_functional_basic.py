f331db776249e4a009e128c428ef57c1cc8d0afe
Richard Purdie <richard.purdie@linuxfoundation.org>
2024-10-18 14:51:31 +0100

    fixups
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- lib/toaster/tests/functional/test_functional_basic.py
+++ lib/toaster/tests/functional/test_functional_basic.py
@@ -88,7 +88,7 @@ class FuntionalTestBasic(SeleniumFunctionalTestCase):
         project_URL=self.get_URL()
 
         # Machine section of page
-        self.wait_until_present('#machine-section')
+        self.wait_until_visible('#machine-section')
         self.assertTrue(self.element_exists('#machine-section'),'Machine section for the project configuration page does not exist')
         self.assertTrue(re.search("qemux86-64",self.driver.find_element(By.XPATH, "//span[@id='project-machine-name']").text),'The machine type is not assigned')
         try:
@@ -100,7 +100,7 @@ class FuntionalTestBasic(SeleniumFunctionalTestCase):
            self.fail(msg='The machine information is wrong in the configuration page')
 
         # Most built recipes section
-        self.wait_until_present('#no-most-built')
+        self.wait_until_visible('#no-most-built')
         try:
            self.driver.find_element(By.ID, 'no-most-built')
         except:
@@ -110,7 +110,7 @@ class FuntionalTestBasic(SeleniumFunctionalTestCase):
         self.assertTrue(re.search("Yocto Project master",self.driver.find_element(By.XPATH, "//span[@id='project-release-title']").text), 'The project release is not defined in the project detail page')
 
         # List of layers in project
-        self.wait_until_present('#layer-container')
+        self.wait_until_visible('#layer-container')
         self.driver.find_element(By.XPATH, "//div[@id='layer-container']")
         self.assertTrue(re.search("3",self.driver.find_element(By.ID, "project-layers-count").text),'There should be 3 layers listed in the layer count')
         try:
@@ -135,8 +135,9 @@ class FuntionalTestBasic(SeleniumFunctionalTestCase):
         self.load_projects_page_helper()
         self.find_element_by_link_text_in_table('projectstable', 'selenium-project').click()
 
-        self.wait_until_present('#machine-section')
+        self.wait_until_visible('#machine-section')
         self.assertTrue(self.element_exists('#machine-section'),'Machine section for the project configuration page does not exist')
+        self.wait_until_visible('#project-machine-name')
         self.assertTrue(re.search("qemux86-64",self.driver.find_element(By.ID, "project-machine-name").text),'The machine type is not assigned')
         try:
             self.driver.find_element(By.ID, "change-machine-toggle").click()
@@ -154,13 +155,13 @@ class FuntionalTestBasic(SeleniumFunctionalTestCase):
         self.wait_until_present('#config-nav')
         project_URL=self.get_URL()
 
-        self.wait_until_present('#no-most-built')
+        self.wait_until_visible('#no-most-built')
         self.assertTrue(re.search("You haven't built any recipes yet",self.driver.find_element(By.ID, "no-most-built").text),'Default message of no builds is not present')
         try:
             self.driver.find_element(By.XPATH, "//div[@id='no-most-built']/p/a[@href="+'"'+project_URL+'images/"'+"]").click()
         except:
             self.fail(msg='No Most built information in project detail page')
-        self.wait_until_present('#config-nav')
+        self.wait_until_visible('#config-nav')
         self.assertTrue(re.search("Compatible image recipes",self.driver.find_element(By.XPATH, "//div[@class='col-md-10']").text),'The Choose a recipe to build link  is not working  properly')
 
 #   testcase (1519)
@@ -168,7 +169,7 @@ class FuntionalTestBasic(SeleniumFunctionalTestCase):
         self.get(reverse('all-projects'))
         self.load_projects_page_helper()
         self.find_element_by_link_text_in_table('projectstable', 'selenium-project').click()
-        self.wait_until_present('#project-release-title')
+        self.wait_until_visible('#project-release-title')
         self.assertTrue(re.search("Yocto Project master",self.driver.find_element(By.ID, "project-release-title").text), 'No project release title information in project detail page')
 
 #   testcase (1520)
@@ -178,7 +179,9 @@ class FuntionalTestBasic(SeleniumFunctionalTestCase):
         self.find_element_by_link_text_in_table('projectstable', 'selenium-project').click()
         self.wait_until_present('#config-nav')
         project_URL=self.get_URL()
+        self.wait_until_visible('#layer-container')
         self.driver.find_element(By.XPATH, "//div[@id='layer-container']")
+        self.wait_until_visible('#project-layers-count')
         self.assertTrue(re.search("3",self.driver.find_element(By.ID, "project-layers-count").text),'There should be 3 layers listed in the layer count')
 
         try:
@@ -213,7 +216,7 @@ class FuntionalTestBasic(SeleniumFunctionalTestCase):
         self.wait_until_present('#config-nav')
         project_URL=self.get_URL()
         self.driver.find_element(By.XPATH, "//div[@id='project-topbar']/ul[@class='nav nav-tabs']/li[@id='topbar-configuration-tab']/a[@href="+'"'+project_URL+'"'+"]").click()
-        self.wait_until_present('#topbar-configuration-tab')
+        self.wait_until_visible('#topbar-configuration-tab')
         self.assertTrue(re.search("Configuration",self.driver.find_element(By.XPATH, "//div[@id='project-topbar']/ul[@class='nav nav-tabs']/li[@id='topbar-configuration-tab']/a[@href="+'"'+project_URL+'"'+"]").text), 'Configuration tab in project topbar is misspelled')
 
         try:
