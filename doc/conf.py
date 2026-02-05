1d0d4a0066603461eacb92a766fec616c1e91257
Antonin Godard <antonin.godard@bootlin.com>
2026-02-05 16:25:45 +0100

    doc: fix the switchers menu
    Fix the switchers.js script of Bitbake to show what are the supported
    versions and be able to switch between them.
    
    The default landing page is the stable branch and shows the BitBake
    version along with the corresponding Yocto Project codename. This
    hopefully makes it easier to remember the correspondance between the
    BitBake version and the Yocto Project version.
    
    This works thanks to a setversions.py script, imported and executed in
    conf.py, which is largely inspired from the one in yocto-docs. It reads
    the tags from the repository and tries to guess the currently checked
    out version of BitBake on which we are.
    
    The "obsolete" warning is now also shown when browsing outdated manuals,
    meaning any version not part of activereleases in setversions.py and
    "dev"/"next".
    
    Signed-off-by: Antonin Godard <antonin.godard@bootlin.com>
    Signed-off-by: Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- doc/conf.py
+++ doc/conf.py
@@ -6,20 +6,16 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-import sys
 import datetime
+import os
+import sys
 
 from pathlib import Path
 
-current_version = "dev"
+sys.path.insert(0, os.path.abspath('.'))
+import setversions
+
+current_version = setversions.get_current_version()
 
 # String used in sidebar
 version = 'Version: ' + current_version
@@ -27,6 +23,10 @@ if current_version == 'dev':
     version = 'Version: Current Development'
 # Version seen in documentation_options.js and hence in js switchers code
 release = current_version
+
+setversions.write_switchers_js("sphinx-static/switchers.js.in",
+                               "sphinx-static/switchers.js",
+                               current_version)
 
 # -- Project information -----------------------------------------------------
 
